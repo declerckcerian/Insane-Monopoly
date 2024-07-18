c5dd7c7072049ec562bdd8968c9d7f21ccf44f6e
StijnVandepitte <stijnvandepitte7@gmail.com>
2024-07-18 22:48:31 +0200

    Update game.py

--- Models/game.py
+++ Models/game.py
@@ -1,3 +1,12 @@
-import .board 
-import .properties
-import .player
+from .board import Board
+from .properties import Properties
+from .player import Player
+from .dice import Dice
+
+class Game:
+  def __init__(self, number_of_players): 
+    players = {}
+    for i in range(number_of_players):
+      players[ "p" + f"{i+1}"] = Player()
+    
+      
